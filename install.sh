352df2b9b324cb4cfa95cf502eef26dd0fedc999
sahilgoyal7214 <114812731+sahilgoyal7214@users.noreply.github.com>
2025-03-24 00:41:10 +0530

    hardware deployment setup

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+sudo apt install git-lfs git 
+python -m venv GanCV
+source GanCV/bin/activate
 pip install -r requirements.txt
 git lfs install
 git clone https://huggingface.co/unicorn441/Image-captioning
